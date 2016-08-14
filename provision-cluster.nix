ce7a8224731f1f665bbb7d54131123977f689099
Richard Lupton <richard.lupton@gmail.com>
2016-08-14 23:17:37 +0100

    Modified to use more up to date docker-compose.

--- provision-cluster.nix
+++ provision-cluster.nix
@@ -1,9 +1,10 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i sh -p python27Packages.docker_compose
+#!nix-shell default.nix -i sh 
 
 # First build the image
-nix-build node.nix -A image
+nix-build node.nix -A image 
 docker load < result
 
 # Then run some number of copies of it
+docker-compose up
 docker-compose scale node=$1
