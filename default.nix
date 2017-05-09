c5796a20b7e7a9e6434c96c41c816ae8bd790e2e
Richard Lupton <richard.lupton@gmail.com>
2017-05-10 00:58:55 +0100

    Small tidy

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { pkgs ? import <nixpkgs> {} , unstable ? import (builtins.fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz) {} }:
 with pkgs;
 stdenv.mkDerivation rec {
-  name = "Clusterfuck";
+  name = "Clustertest";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
     unstable.python27Packages.docker_compose
